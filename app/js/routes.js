58dcb1517d4e45f8262917905dc5024e4c084234
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2014-02-23 22:23:07 -0800

    initial commit

---
+++ app/js/routes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+angular.module('myApp.routes', ['ngRoute'])
+
+   // configure views; the authRequired parameter is used for specifying pages
+   // which should only be available while logged in
+   .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/notes', {
+         authRequired: true, // must authenticate before viewing this page
+         templateUrl: 'partials/note.html',
+         controller: 'NoteCtrl'
+      });
+
+      $routeProvider.when('/login', {
+         templateUrl: 'partials/login.html',
+         controller: 'LoginCtrl'
+      });
+
+      $routeProvider.otherwise({redirectTo: '/notes'});
+   }]);
