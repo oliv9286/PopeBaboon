41bf5923545cb0aebc6efea04a783486574ca014
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2014-02-25 01:31:24 -0800

    added 3 way binding to save notes automatically

---
+++ config/app/js/services.js
@@ -0,0 +1,9 @@
+(function() {
+   'use strict';
+
+   /* Services */
+
+   angular.module('myApp.services', ['myApp.service.login', 'myApp.service.firebase', 'myApp.service.changeEmail'])
+
+})();
+
