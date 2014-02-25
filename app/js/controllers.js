8b68b0576684d7bdc5bdafe2d0846f49e9dcc968
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2014-02-25 09:08:10 -0800

    fixed line break issue

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,15 +7,6 @@ angular.module('myApp.controllers', [])
     $scope.pass = null;
     $scope.confirm = null;
 
-
-
-    // $('#content').keydown(function(){
-    //     console.log('keydown');
-    //     if(!$(this).text().trim()){
-    //       setCursorToEnd($(this));
-    //     }
-    // });
-
     /* Email and password login */
     $scope.login = function(cb) {
        $scope.err = null;
@@ -115,7 +106,6 @@ angular.module('myApp.controllers', [])
          }
       );
 
-
    $scope.notes = syncData(['users', $scope.auth.user.uid, 'notes']);
 
    /* count the number of notes that the user owns, display empty message if zero */
@@ -130,17 +120,12 @@ angular.module('myApp.controllers', [])
   }
 
   /* sets the cursor for contenteditable to the end*/
-  $scope.setCursorToEnd = function(ele)
+  $scope.setCursorToEnd = function(keycode,ele)
   {
-
-    console.log('keydown');
-    var range = document.createRange();
-    var sel= window.getSelection();
-    range.setStart(ele, 1);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-    ele.focus();
+    //dismiss if key pressed is space or up arrow
+    if (keycode == 13 || keycode == 40){
+      return;
+    }
   }
 
   $scope.countNotes();
@@ -262,20 +247,11 @@ angular.module('myApp.controllers', [])
        $('#mainTab a[data-target="#notebook"]').tab('show');
   }
 
-    // $scope.syncAccount = function() {
-    //    $scope.user = {};
-    //    syncData(['users', $scope.auth.user.uid]).$bind($scope, 'user').then(function(unBind) {
-    //       $scope.unBindAccount = unBind;
-    //    });
-    // };
-    // // set initial binding
-    // $scope.syncAccount();
-
-    $scope.testBinding = function () {
+    $scope.bindNotes = function () {
       syncData(['users', $scope.auth.user.uid, 'notes']).$bind($scope, 'notes');
     }
 
-    $scope.testBinding();
+    $scope.bindNotes();
 
     $scope.logout = function() {
        loginService.logout();
